e6b997655cb0232b397b20c85031cb663bcc5615
marcossbalbino <161628490+marcossbalbino@users.noreply.github.com>
2025-02-26 14:50:02 -0300

    aula 01

---
+++ monster/code.js
@@ -0,0 +1,16 @@
+
+function banco(){
+   const bd = [
+                {id:1,login:"john", senha:"1234",nome:"john",email:"john@gmail.com"}, //0
+                {id:2,login:"ringo", senha:"1234@",nome:"ringo",email:"ringo@gmail.com"}, //1
+                {id:3,login:"paul", senha:"12345",nome:"paul",email:"paul@gmail.com"}//2
+            ]
+    let dados = JSON.stringify(bd)
+
+    localStorage.setItem("banco", dados)
+
+console.log(dados)
+    
+}
+
+
